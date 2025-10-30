4c85035ede04537e525870af23006fafa282e1ac
ANSH V <ansh2007fhdy@gmail.com>
2025-10-30 21:07:49 +0530

    Refactor blog schema definition in blog.js

--- models/blog.js
+++ models/blog.js
@@ -1,10 +1,12 @@
 const mongoose=require('mongoose');
 const Schema = mongoose.Schema;
+//this is the schema for the blogs
 const blogSchema=new mongoose.Schema({
     title:{
         type:String,
         required:true,
     },
+    
     body:{
         type:String,
         required:true, 
@@ -42,4 +44,5 @@ const Blog=mongoose.model('blog',blogSchema);
 //ALL EXPORTS OF THIS FILE----------------------------------------------------
 module.exports={
     Blog,
+
 };
